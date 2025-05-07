b9b77f4d5c131beb4a780f019a066bb265676b80
Olga Mazurova <vsilena@mail.ru>
2025-05-07 14:40:43 +0300

    Изменена структура проекта.

--- app/bot/dialogs/start/dialogs.py
+++
@@ -1,9 +0,0 @@
-from aiogram_dialog import Dialog, Window
-from aiogram_dialog.widgets.text import Format
-
-from app.bot.dialogs.start.getters import get_hello
-from app.bot.states.start import StartSG
-
-start_dialog = Dialog(
-    Window(Format("{hello}"), getter=get_hello, state=StartSG.start),
-)
--- config/config.py
+++
@@ -1,9 +0,0 @@
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix=False,  # "DYNACONF",
-    environments=True,  # Автоматически использовать секцию текущей среды
-    env_switcher="ENV_FOR_DYNACONF",
-    settings_files=["settings.toml", ".secrets.toml"],
-    load_dotenv=True,
-)
